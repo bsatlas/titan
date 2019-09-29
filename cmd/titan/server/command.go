cc5f57ef25ad1add7397c2e9e478f9b0e9190324
Atlas Kerr <atlaskerr@gmail.com>
2019-09-29 16:11:55 -0500

    feat(cli): hookup server command to main handler
    Signed-off-by: Atlas Kerr <atlaskerr@gmail.com>

--- cmd/titan/server/command.go
+++ cmd/titan/server/command.go
@@ -1,9 +1,8 @@
 package server
 
 import (
-	"fmt"
-
 	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
 )
 
 // Command is the entrypoint for starting the titan registry server.
@@ -12,12 +11,31 @@ func Command() *cobra.Command {
 		Use:  "server",
 		RunE: runE,
 	}
-	flags := cmd.Flags()
-	flags.StringP("config", "c", "config.json", "path to titan server config file")
+	setFlags(cmd.Flags())
 	return cmd
 }
 
+func setFlags(flags *pflag.FlagSet) {
+	// config file flag
+	flags.StringP(
+		"config",
+		"c",
+		"config.json",
+		"path to titan server config file",
+	)
+}
+
+func configFromFlags(flags *pflag.FlagSet) {}
+
 func runE(cmd *cobra.Command, args []string) error {
-	fmt.Println("server command")
+	srv, err := server()
+	if err != nil {
+		return err
+	}
+
+	err = runServer(srv)
+	if err != nil {
+		return err
+	}
 	return nil
 }
